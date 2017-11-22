47952a8350a3e1c11e0110c7680901c3983cb448
snyh <snyh@snyh.org>
2017-11-22 11:48:41 +0800

    feat: implement a simple page cache usage reporter

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func T(fname string) error {
+	info, err := FileMincore(fname)
+	if err != nil {
+		return err
+	}
+	if info.Percentage() > 0 {
+		fmt.Println(info)
+	}
+	return nil
+}
+
+func main() {
+	for _, f := range os.Args[1:] {
+		e := T(f)
+		if e != nil {
+			fmt.Println("E:", e)
+		}
+	}
+}
---
+++ record.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/sys/unix"
+	"os"
+	"path/filepath"
+	"unsafe"
+)
+
+type FileCacheInfo struct {
+	FName   string
+	InCache []bool
+}
+
+var ZeroFileInfo = FileCacheInfo{}
+
+func (info FileCacheInfo) String() string {
+	return fmt.Sprintf("%0.1f%% of %dKB %s",
+		info.Percentage(),
+		len(info.InCache)*os.Getpagesize()/1024,
+		info.FName,
+	)
+}
+
+func (info FileCacheInfo) Percentage() float32 {
+	var c int
+	for _, i := range info.InCache {
+		if i {
+			c++
+		}
+	}
+	return 100 * float32(c) / float32(len(info.InCache))
+}
+
+func FileMincore(fname string) (FileCacheInfo, error) {
+	fname, err := filepath.Abs(fname)
+	if err != nil {
+		return ZeroFileInfo, err
+	}
+	f, err := os.Open(fname)
+	if err != nil {
+		return ZeroFileInfo, err
+	}
+	defer f.Close()
+
+	info, err := f.Stat()
+	if err != nil {
+		return ZeroFileInfo, err
+	}
+	if info.IsDir() {
+		return ZeroFileInfo, nil
+	}
+
+	size := info.Size()
+
+	//skip could not mmap error when the file size is 0
+	if int(size) == 0 {
+		return ZeroFileInfo, nil
+	}
+	// mmap is a []byte
+	mmap, err := unix.Mmap(int(f.Fd()), 0, int(size), unix.PROT_NONE, unix.MAP_SHARED)
+	if err != nil {
+		return ZeroFileInfo, fmt.Errorf("could not mmap %s: %v", fname, err)
+	}
+	// TODO: check for MAP_FAILED which is ((void *) -1)
+	// but maybe unnecessary since it looks like errno is always set when MAP_FAILED
+
+	// one byte per page, only LSB is used, remainder is reserved and clear
+	vecsz := (size + int64(os.Getpagesize()) - 1) / int64(os.Getpagesize())
+	vec := make([]byte, vecsz)
+
+	// get all of the arguments to the mincore syscall converted to uintptr
+	mmap_ptr := uintptr(unsafe.Pointer(&mmap[0]))
+	size_ptr := uintptr(size)
+	vec_ptr := uintptr(unsafe.Pointer(&vec[0]))
+
+	ret, _, err := unix.Syscall(unix.SYS_MINCORE, mmap_ptr, size_ptr, vec_ptr)
+	if ret != 0 {
+		return ZeroFileInfo, fmt.Errorf("syscall SYS_MINCORE failed: %v", err)
+	}
+	defer unix.Munmap(mmap)
+
+	mc := make([]bool, vecsz)
+
+	for i, b := range vec {
+		if b%2 == 1 {
+			mc[i] = true
+		} else {
+			mc[i] = false
+		}
+	}
+
+	return FileCacheInfo{
+		FName:   fname,
+		InCache: mc,
+	}, nil
+}
