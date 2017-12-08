6dfe0231975635e0a66d256c69eb9f1883e11fba
snyh <snyh@snyh.org>
2017-12-08 11:04:46 +0800

    feat: measure dump time consuming

--- Vagrantfile
+++ Vagrantfile
@@ -12,17 +12,17 @@ Vagrant.configure("2") do |config|
 
   # Every Vagrant development environment requires a box. You can search for
   # boxes at https://atlas.hashicorp.com/search.
-  config.vm.box = "debian/stretch64"
+  config.vm.box = "debian/9/vbguest"
 #  config.vm.box = "deepin15.4"
 
-  config.vm.synced_folder ".", "/vagrant", type: "virtualbox"
+  config.vm.synced_folder "./kernel", "/vagrant", type: "virtualbox"
   # config.vm.synced_folder ".", "/home/vagrant/work", type: "rsync",
   #                         rsync__exclude: ".git/"
 
   # Disable automatic box update checking. If you disable this, then
   # boxes will only be checked for updates when the user runs
   # `vagrant box outdated`. This is not recommended.
-  # config.vm.box_check_update = false
+  config.vm.box_check_update = false
 
   # Create a forwarded port mapping which allows access to a specific port
   # within the machine from a port on the host machine. In the example below,
@@ -66,6 +66,8 @@ Vagrant.configure("2") do |config|
   #   push.app = "YOUR_ATLAS_USERNAME/YOUR_APPLICATION_NAME"
   # end
 
+  # config.vbguest.auto_update = false
+
   # Enable provisioning with a shell script. Additional provisioners such as
   # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
   # documentation for more information about their specific syntax and use.
@@ -73,4 +75,7 @@ Vagrant.configure("2") do |config|
   #   apt-get update
   #   apt-get install -y apache2
   # SHELL
+  config.vm.provision "shell", inline: <<-SHELL
+    cd /vagrant >> /home/vagrant/.bashrc
+  SHELL
 end
--- kernel/test_module.c
+++ kernel/test_module.c
@@ -6,59 +6,63 @@
 #include <linux/fs.h>
 #include <linux/mm.h>
 #include <linux/list_lru.h>
+#include <linux/time.h>
 
-static void is_dentry_in_cache(void);
+
+static bool dump_inode(struct inode *inode);
+static void traver_sb(struct super_block *sb, void *user_data);
+static void show_time_elapse(struct timespec begin);
+
+static bool SHOW = false;
 
 
 static int test_module_init(void)
 {
+  struct file_system_type* t = get_fs_type("ext4");
+
   printk("Test snyh haha Module Installed\n");
-  is_dentry_in_cache();
+
+  iterate_supers_type(t, traver_sb, 0);
+  //put_filesystem(t);
   return 0;
 }
 
-void traver_fn2(struct super_block *sb, void *user_data)
+enum lru_status inode_lru_walk (struct list_head *item, struct list_lru_one *list,
+                                spinlock_t *lock, void *cb_arg)
 {
-  struct inode *inode = NULL;
-  struct dentry *d = NULL;
-    unsigned long n = 0;
-    static char bufname[1024];
-    spin_lock(&sb->s_inode_list_lock);
-    list_for_each_entry(inode, &sb->s_inodes, i_sb_list) {
-        d = d_find_any_alias(inode);
-        if (d == 0) {
-          continue;
-        }
-
-        spin_lock(&inode->i_lock);
-
-        if (!S_ISREG(inode->i_mode) || inode->i_mapping->nrpages == 0) {
-          spin_unlock(&inode->i_lock);
-          dput(d);
-          continue;
-        }
-        n++;
-
-        spin_unlock(&inode->i_lock);
-        sector_t bn = bmap(inode, 0);
-        spin_lock(&inode->i_lock);
-
-        printk("Test %ld\t%ld\t%ld\t%ld\t%s\t%ld\n", n, inode->i_ino,
-               inode->i_mapping->nrpages,
-               (((loff_t)inode->i_blocks) << 9) + inode->i_bytes,
-               dentry_path_raw(d, bufname, sizeof(bufname)),
-               bn
-               );
-        dput(d);
-        spin_unlock(&inode->i_lock);
-    }
-    spin_unlock(&sb->s_inode_list_lock);
+  unsigned long *count = cb_arg;
+  struct inode* inode = list_entry(item, struct inode, i_lru);
+  if (dump_inode(inode)) (*count)++;
+  return LRU_SKIP;
 }
 
-static void is_dentry_in_cache(void)
+void collect_inode_in_lru(struct super_block *sb, unsigned long* count)
 {
-  struct file_system_type* t = get_fs_type("ext4");
-  iterate_supers_type(t, traver_fn2, 0);
+  unsigned long n = 10000000;
+  list_lru_walk(&(sb->s_inode_lru), inode_lru_walk, count, n);
+}
+
+static void traver_sb(struct super_block *sb, void *user_data)
+{
+  struct timespec begin = CURRENT_TIME;
+  struct inode *inode = NULL;
+  unsigned long n1 = 0, n2 = 0;
+
+  spin_lock(&sb->s_inode_list_lock);
+  list_for_each_entry(inode, &sb->s_inodes, i_sb_list) {
+    if (dump_inode(inode)) n1++;
+  }
+  spin_unlock(&sb->s_inode_list_lock);
+  collect_inode_in_lru(sb, &n2);
+
+  printk("For %s: %ld %ld", sb->s_id, n1, n2);
+  show_time_elapse(begin);
+}
+
+static void show_time_elapse(struct timespec begin)
+{
+  struct timespec now = CURRENT_TIME;
+  printk("Elapse %lld.%.9lds\n", (long long)(now.tv_sec - begin.tv_sec), now.tv_nsec - begin.tv_nsec);
 }
 
 static void test_module_exit(void)
@@ -72,3 +76,37 @@ module_exit(test_module_exit);
 MODULE_AUTHOR("IsonProjects");
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Simple Kernel Module");
+
+static bool dump_inode(struct inode *inode)
+{
+  struct dentry *d = NULL;
+  sector_t bn = 0;
+  static char bufname[1024];
+
+  d = d_find_any_alias(inode);
+  if (d == 0) {
+    return false;
+  }
+
+  spin_lock(&inode->i_lock);
+  if (!S_ISREG(inode->i_mode) || inode->i_mapping->nrpages == 0) {
+    spin_unlock(&inode->i_lock);
+    dput(d);
+    return false;
+  }
+
+  /* spin_unlock(&inode->i_lock); */
+  /* bn = bmap(inode, 0); */
+  /* spin_lock(&inode->i_lock); */
+
+  if (SHOW)
+    printk("Inode:%ld\t%ld\t%lld\t%s\t%ld\n", inode->i_ino,
+           inode->i_mapping->nrpages,
+           ((((loff_t)inode->i_blocks) << 9) + inode->i_bytes) / 4096,
+           dentry_path_raw(d, bufname, sizeof(bufname)),
+           bn
+           );
+  dput(d);
+  spin_unlock(&inode->i_lock);
+  return true;
+}
