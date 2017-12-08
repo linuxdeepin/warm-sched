1ab139a85a94deee3adb9fdb8b87f951ac640840
snyh <snyh@snyh.org>
2017-12-08 14:27:23 +0800

    feat: dump collect information to /proc/snyh123

--- kernel/test_module.c
+++ kernel/test_module.c
@@ -1,31 +1,42 @@
 #include <linux/module.h>
 #include <linux/dcache.h>
-#include <linux/kallsyms.h>
-#include <linux/pagemap.h>
-#include <linux/slab.h>
 #include <linux/fs.h>
 #include <linux/mm.h>
 #include <linux/list_lru.h>
 #include <linux/time.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
 
 
 static bool dump_inode(struct inode *inode);
 static void traver_sb(struct super_block *sb, void *user_data);
 static void show_time_elapse(struct timespec begin);
 
-static bool SHOW = false;
+static struct proc_dir_entry *proc_file_entry;
+static struct seq_file* SF = 0;
 
-
-static int test_module_init(void)
+static int show_proc_content(struct seq_file *filp, void *p)
 {
   struct file_system_type* t = get_fs_type("ext4");
-
-  printk("Test snyh haha Module Installed\n");
-
+  SF = filp;
   iterate_supers_type(t, traver_sb, 0);
+  SF = 0;
   //put_filesystem(t);
   return 0;
 }
+
+static int proc_open_callback(struct inode *inode, struct file *filp)
+{
+  return single_open(filp, show_proc_content, 0);
+}
+
+static const struct file_operations proc_file_fops = {
+  .owner = THIS_MODULE,
+  .open = proc_open_callback,
+  .read	= seq_read,
+  .llseek = seq_lseek,
+  .release = single_release,
+};
 
 enum lru_status inode_lru_walk (struct list_head *item, struct list_lru_one *list,
                                 spinlock_t *lock, void *cb_arg)
@@ -55,18 +66,29 @@ static void traver_sb(struct super_block *sb, void *user_data)
   spin_unlock(&sb->s_inode_list_lock);
   collect_inode_in_lru(sb, &n2);
 
-  printk("For %s: %ld %ld", sb->s_id, n1, n2);
+  seq_printf(SF, "%s\t%ld\t%ld\t", sb->s_id, n1, n2);
   show_time_elapse(begin);
 }
 
 static void show_time_elapse(struct timespec begin)
 {
   struct timespec now = CURRENT_TIME;
-  printk("Elapse %lld.%.9lds\n", (long long)(now.tv_sec - begin.tv_sec), now.tv_nsec - begin.tv_nsec);
+  seq_printf(SF, "%lld.%.9lds\n", (long long)(now.tv_sec - begin.tv_sec), now.tv_nsec - begin.tv_nsec);
+}
+
+static int test_module_init(void)
+{
+  printk("Test snyh haha Module Installed\n");
+
+  proc_file_entry = proc_create("snyh123", 0, NULL, &proc_file_fops);
+  if (proc_file_entry == NULL)
+    return -ENOMEM;
+  return 0;
 }
 
 static void test_module_exit(void)
 {
+  remove_proc_entry("snyh123", NULL);
   printk("Test Module Removed\n");
 }
 
@@ -80,7 +102,6 @@ MODULE_DESCRIPTION("Simple Kernel Module");
 static bool dump_inode(struct inode *inode)
 {
   struct dentry *d = NULL;
-  sector_t bn = 0;
   static char bufname[1024];
 
   d = d_find_any_alias(inode);
@@ -95,17 +116,12 @@ static bool dump_inode(struct inode *inode)
     return false;
   }
 
-  /* spin_unlock(&inode->i_lock); */
-  /* bn = bmap(inode, 0); */
-  /* spin_lock(&inode->i_lock); */
-
-  if (SHOW)
-    printk("Inode:%ld\t%ld\t%lld\t%s\t%ld\n", inode->i_ino,
-           inode->i_mapping->nrpages,
-           ((((loff_t)inode->i_blocks) << 9) + inode->i_bytes) / 4096,
-           dentry_path_raw(d, bufname, sizeof(bufname)),
-           bn
-           );
+  if (SF)
+    seq_printf(SF, "%ld\t%ld\t%s\t\n",
+               inode->i_ino,
+               inode->i_mapping->nrpages,
+               dentry_path_raw(d, bufname, sizeof(bufname))
+               );
   dput(d);
   spin_unlock(&inode->i_lock);
   return true;
